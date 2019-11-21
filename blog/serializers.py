08846a0d3c99fa3c7883a2a08e3c5e6a26f93560
iv <consmebius@gmail.com>
2019-11-21 17:54:52 +0200

    .

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,10 +3,19 @@ from taggit_serializer.serializers import (TagListSerializerField, TaggitSeriali
 
 class ArticleSerializer(serializers.Serializer):
     title = serializers.CharField()
+    slug = serializers.SlugField()
     article = serializers.CharField()
     status = serializers.CharField()
     tags = TagListSerializerField()
     created = serializers.DateTimeField()
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Article.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.slug = validated_data.get('slug', instance.slug)
+    #     instance.article = validated_data.get('article', instance.article)
+    #     instance.tags = validated_data.get('tags', instance.tags)
+    #     instance.save()
+    #     return instance
--- blog/views.py
+++ blog/views.py
@@ -16,10 +16,20 @@ class ArticleView(APIView):
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
 
-    def post(self, request):
-        article = request.data.get('article')
-        # Create an article from the above data
-        serializer = ArticleSerializer(data=article)
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+    # def post(self, request):
+    #     article = request.data.get('article')
+    #     # Create an article from the above data
+    #     serializer = ArticleSerializer(data=article)
+    #     if serializer.is_valid(raise_exception=True):
+    #         article_saved = serializer.save()
+    #     return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    # def put(self, request, slug):
+    #     saved_article = get_object_or_404(Article.objects.all(), slug=slug)
+    #     data = request.data.get('article')
+    #     serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+    #     if serializer.is_valid(raise_exception=True):
+    #         article_saved = serializer.save()
+    #     return Response({
+    #         "success": "Article '{}' updated successfully".format(article_saved.title)
+    #     })
