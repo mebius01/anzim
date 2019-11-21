8c4598b32eab7ca0da477b92532edc3e4192058c
iv <consmebius@gmail.com>
2019-11-21 23:11:43 +0200

    .

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,21 +2,12 @@ from rest_framework import serializers
 from .models import Article
 from taggit_serializer.serializers import (TagListSerializerField, TaggitSerializer)
 
-<<<<<<< HEAD
 class ArticleSerializer(serializers.ModelSerializer):
-=======
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    slug = serializers.SlugField()
-    article = serializers.CharField()
-    status = serializers.CharField()
->>>>>>> 08846a0d3c99fa3c7883a2a08e3c5e6a26f93560
     tags = TagListSerializerField()
     class Meta:
         model = Article
         fields = ['id', 'title', 'article', 'status', 'tags']
 
-<<<<<<< HEAD
 # class ArticleSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField()
@@ -48,15 +39,3 @@ class ArticleSerializer(serializers.Serializer):
 
 #     def create(self, validated_data):
 #         return Article.objects.create(**validated_data)
-=======
-    # def create(self, validated_data):
-    #     return Article.objects.create(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.slug = validated_data.get('slug', instance.slug)
-    #     instance.article = validated_data.get('article', instance.article)
-    #     instance.tags = validated_data.get('tags', instance.tags)
-    #     instance.save()
-    #     return instance
->>>>>>> 08846a0d3c99fa3c7883a2a08e3c5e6a26f93560
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-<<<<<<< HEAD
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
@@ -87,33 +86,3 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
 #     #         article_saved = serializer.save()
 #     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 #     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
-# def home(request):
-# 	articles = Article.objects.all()
-# 	return render(request, 'blog/home.html', {'articles':articles})
-# Create your views here.
-
-class ArticleView(APIView):
-    def get(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response({"articles": serializer.data})
-
-    # def post(self, request):
-    #     article = request.data.get('article')
-    #     # Create an article from the above data
-    #     serializer = ArticleSerializer(data=article)
-    #     if serializer.is_valid(raise_exception=True):
-    #         article_saved = serializer.save()
-    #     return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
-
-    # def put(self, request, slug):
-    #     saved_article = get_object_or_404(Article.objects.all(), slug=slug)
-    #     data = request.data.get('article')
-    #     serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
-    #     if serializer.is_valid(raise_exception=True):
-    #         article_saved = serializer.save()
-    #     return Response({
-    #         "success": "Article '{}' updated successfully".format(article_saved.title)
-    #     })
->>>>>>> 08846a0d3c99fa3c7883a2a08e3c5e6a26f93560
