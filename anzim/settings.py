52ae044b840d45c189d1c94b3345435073dad2b4
iv <consmebius@gmail.com>
2019-12-26 17:32:48 +0200

    add tag-list

--- anzim/settings.py
+++ anzim/settings.py
@@ -90,6 +90,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'blog.context_processors.title_list.title_list',
+                'blog.context_processors.title_list.tag_list',
             ],
         },
     },
--- blog/context_processors/title_list.py
+++ blog/context_processors/title_list.py
@@ -1,3 +1,6 @@
 from blog.models import Article
 def title_list(request):
 	return {'title_list': Article.objects.all().order_by('-publish')}
+
+def tag_list(request):
+	return {'tag_list': Article.tags.most_common()}
