afb4d4e49863b07e8f6ab829ac656097390400eb
iv <consmebius@gmail.com>
2020-01-14 09:34:29 +0200

    .

--- blog/context_processors/title_list.py
+++ blog/context_processors/title_list.py
@@ -1,6 +1,6 @@
 from blog.models import Article
 def title_list(request):
-	return {'title_list': Article.objects.all().order_by('-publish')}
+	return {'title_list': Article.objects.all().order_by('-updated')}
 
 def tag_list(request):
 	return {'tag_list': Article.tags.most_common()}
