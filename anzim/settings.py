b9f1ba19a171f6acb68a4f11f71f92bfc6715c65
iv <consmebius@gmail.com>
2020-03-30 22:34:59 +0300

    tag PRE fix..

--- anzim/settings.py
+++ anzim/settings.py
@@ -176,45 +176,14 @@ EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True)
 
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
-# CKEDITOR_CONFIGS = {
-#     'default': {
-#         'skin': 'moono',
-#         # uncomment to restrict only those languages
-#         # 'codeSnippet_languages': {
-#         #     'python': 'Python Guru',
-#         #     'javascript': 'JavaScript Fu',
-#         #     'php': 'PHP Ninja',
-#         #     'c': 'You custom funny language name'
-#         # },
-#         'toolbar': 'MyCustomToolbar',
-#         'extraPlugins': ','.join(
-#             [
-#                 # add the follow plugins
-#                 'codesnippet',
-#                 'widget',
-#                 'dialog',
-#             ]),
-#     }
-# }
-
-# CKEDITOR_CONFIGS = {
-#    'default': {
-#         'toolbar':[ ['CodeSnippet', ], ],
-#         'height': 400,
-#         'width': 900,
-#         'removePlugins': 'stylesheetparser',
-#         'extraPlugins': 'codesnippet',
-#    },
-# }
-
+# CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 CKEDITOR_CONFIGS = {
     # https://vivazzi.pro/it/ckeditor-config/
     'default': {
         'toolbar': [
             [
             '-', 'Source',
-            'Undo', 'Redo',
+            '-', 'Undo', 'Redo',
             '-', 'Bold', 'Italic', 'Underline',
             '-', 'Link', 'Unlink', 'Anchor',
             '-', 'Format', 'FontSize',
@@ -232,75 +201,29 @@ CKEDITOR_CONFIGS = {
             ['CodeSnippet', ],
         ],
         'extraPlugins': 'codesnippet',
+        'codeSnippet_languages': {
+            'bash': 'Bash',
+            'python': 'Python',
+            'javascript': 'JavaScript',
+            'html': 'HTML',
+            'json': 'Json',
+            'css': 'CSS',
+            'scss': 'Scss',
+            'less': 'Less',
+            'apache': 'Apache',
+            'nginx': 'Nginx',
+            'http': 'HTTP',
+            'SQL' : 'SQL',
+            'makefile': 'Makefile',
+            'shell': 'Shell Session',
+        },
+        'codeSnippet_theme': 'monokai',
+        'disableNativeSpellChecker': False,
+        'removePlugins': 'liststyle, tabletools, scayt, menubutton, contextmenu',
+        'language': 'ru',
         'height': 500,
         'width': '100%',
         'toolbarCanCollapse': False,
         'forcePasteAsPlainText': True,
     }
 }
-
-
-# CKEDITOR_CONFIGS = {
-#     'default': {
-#         'skin': 'moono',
-#         # 'skin': 'office2013',
-#         'toolbar_Basic': [
-#             ['Source', '-', 'Bold', 'Italic']
-#         ],
-#         'toolbar_YourCustomToolbarConfig': [
-#             {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']},
-#             {'name': 'clipboard', 'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-#             {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
-#             {'name': 'forms',
-#              'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
-#                        'HiddenField']},
-#             '/',
-#             {'name': 'basicstyles',
-#              'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-#             {'name': 'paragraph',
-#              'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
-#                        'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
-#                        'Language']},
-#             {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
-#             {'name': 'insert',
-#              'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
-#             '/',
-#             {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
-#             {'name': 'colors', 'items': ['TextColor', 'BGColor']},
-#             {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
-#             {'name': 'about', 'items': ['About']},
-#             '/',  # put this to force next toolbar on new line
-#             {'name': 'yourcustomtools', 'items': [
-#                 # put the name of your editor.ui.addButton here
-#                 'Preview',
-#                 'Maximize',
-
-#             ]},
-#         ],
-#         'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
-#         # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
-#         # 'height': 291,
-#         # 'width': '100%',
-#         # 'filebrowserWindowHeight': 725,
-#         # 'filebrowserWindowWidth': 940,
-#         # 'toolbarCanCollapse': True,
-#         # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
-#         'tabSpaces': 4,
-#         'extraPlugins': ','.join([
-#             'uploadimage', # the upload image feature
-#             # your extra plugins here
-#             'div',
-#             'autolink',
-#             'autoembed',
-#             'embedsemantic',
-#             'autogrow',
-#             # 'devtools',
-#             'widget',
-#             'lineutils',
-#             'clipboard',
-#             'dialog',
-#             'dialogui',
-#             'elementspath'
-#         ]),
-#     }
-# }
