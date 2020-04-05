129b6d577ee1fdfa093dd0b85e398c76cb2afa64
iv <consmebius@gmail.com>
2020-04-05 22:04:50 +0300

    add go to top&down

---
+++ static/js/main.js
@@ -0,0 +1,26 @@
+var scrollA = document.getElementById('scrollA');
+var scrollB = document.getElementById('scrollB');
+var heightDoc = document.documentElement.scrollHeight- document.documentElement.clientHeight;
+
+window.addEventListener('scroll', function() {
+    if (document.documentElement.scrollTop > 30) {
+        scrollA.style.display = "block";
+        scrollB.style.display = "block";
+    } else {
+        scrollA.style.display = "none";
+        scrollB.style.display = "none";
+    }
+    if (document.documentElement.scrollTop > heightDoc - 30) {
+        scrollB.style.display = "none";
+    }
+});
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+function downFunction() {
+    document.body.scrollTop = heightDoc;
+    document.documentElement.scrollTop = heightDoc;
+}
