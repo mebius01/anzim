c432d215722e266816ad837c98599b01bdbefb45
iv <consmebius@gmail.com>
2020-04-06 16:06:59 +0300

    js fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,9 @@
 var scrollA = document.getElementById('scrollA');
 var scrollB = document.getElementById('scrollB');
-var heightDoc = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var heightDoc;
 
 window.addEventListener('scroll', function() {
+    heightDoc = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     if (document.documentElement.scrollTop > 30) {
         scrollA.style.display = "block";
         scrollB.style.display = "block";
@@ -13,12 +14,14 @@ window.addEventListener('scroll', function() {
     if (document.documentElement.scrollTop > heightDoc - 30) {
         scrollB.style.display = "none";
     }
+    // console.log(document.documentElement.scrollHeight);
+    // console.log(document.documentElement.clientHeight);
+    // console.log(document.documentElement.scrollTop);
 });
-
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
+}
 
 function downFunction() {
     document.body.scrollTop = heightDoc;
